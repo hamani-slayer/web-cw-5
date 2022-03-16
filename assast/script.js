4fbe5017d80c2c81d50332b8cd288ec48c69df92
hamani-slayer <100710909+hamani-slayer@users.noreply.github.com>
2022-03-16 19:50:02 +0300

    oookkk

--- assast/script.js
+++ assast/script.js
@@ -1,3 +1,9 @@
 alert('abdulrahman if you click you WILL get hacked??')
 console.log('abdulrahman if you click you WILL get hacked??')
 console.log('أنا في الصف الحادي عشر 2/9')
+let budget = 20
+console.log( 30 > budget < 80 )  
+
+budget = 50
+
+console.log( 30 > budget < 80 ) 
